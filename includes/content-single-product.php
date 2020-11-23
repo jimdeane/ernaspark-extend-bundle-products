c4eecec8472bd100b0ac8b20c7ec3b47f21989f6
Jim Deame <jim@deane.com>
2020-11-23 12:09:04 +0000

    repeated group of sub products displaying

--- includes/content-single-product.php
+++ includes/content-single-product.php
@@ -46,52 +46,44 @@ if (post_password_required()) {
 		<div>
 			<div>
 				<div>
-					<h4>Printed Report</h4> main product details
-					<div>DpecialistReprotName</div>
+					<h4>Printed Report</h4> 
+					<div>SpecialistReportName</div>
 					<div>SpecialistReportTitle</div>
 					<div>SpecialistReportIntroText</div>
-					<div>Main report cost quantity and add to cart</div>
+					
+					<?PHP echo wp_kses_post(wc_get_stock_html($product)); ?>
 					<div class="wcbp_bottom">
 						<span>Price : £100.00</span>
-						<?php
-						$qty = $product->get_min_purchase_quantity();
-						if (isset($_POST['wcbp_bundle_products_nonce']) && wp_verify_nonce(wc_clean($_POST['wcbp_bundle_products_nonce']), 'wcbp_bundle_products_nonce')) {
-							$qty = isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : $product->get_min_purchase_quantity();
-						}
-						echo wp_kses_post(wc_get_stock_html($product));
-						if ($product->is_purchasable() && $product->is_in_stock()) {
-							do_action('woocommerce_before_add_to_cart_form'); ?>
 
-							<form class="cart" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post" enctype='multipart/form-data'>
-								<?php //do_action('woocommerce_before_add_to_cart_button'); ?>
-
-								<?php
-								do_action('woocommerce_before_add_to_cart_quantity');
-
-								woocommerce_quantity_input(
-									array(
-										'min_value'   => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
-										'max_value'   => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
-										'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-									)
-								);
-
-								do_action('woocommerce_after_add_to_cart_quantity');
-								?>
-
-								<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
-
-								<?php do_action('woocommerce_after_add_to_cart_button'); ?>
-							</form>
-
-							<?php do_action('woocommerce_after_add_to_cart_form'); 
-						 } ?>
 					</div>
-					<div>
+					<div  style="display: flex; flex-direction: column;">
 						<h4>Individual Articles for Download</h4>
 						<?PHP
 						do_action('ebp_product_layout');
 						?>
+						<div style="display: flex; flex-direction: row;">
+							<div>
+								<?php
+								$qty = $product->get_min_purchase_quantity();
+								if (isset($_POST['wcbp_bundle_products_nonce']) && wp_verify_nonce(wc_clean($_POST['wcbp_bundle_products_nonce']), 'wcbp_bundle_products_nonce')) {
+									$qty = isset($_POST['quantity']) ? wc_stock_amount($_POST['quantity']) : $product->get_min_purchase_quantity();
+								}
+								if ($product->is_purchasable() && $product->is_in_stock()) {
+									do_action('woocommerce_before_add_to_cart_form'); ?>
+							</div>
+							<div>
+								<form class="cart" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post" enctype='multipart/form-data'>
+									<?php
+									do_action('woocommerce_before_add_to_cart_quantity');
+									do_action('woocommerce_after_add_to_cart_quantity');
+									?>
+									<button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+									<?php do_action('woocommerce_after_add_to_cart_button'); ?>
+								</form>
+							<?php do_action('woocommerce_after_add_to_cart_form');
+								} ?>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
